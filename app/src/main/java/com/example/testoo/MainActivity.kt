763af73173280df84158776f3eee2e68bdd421f1
ElmansouriAMINE <amineelmansouri2001@gmail.com>
2024-03-24 22:30:21 +0000

    Mycommit ++

--- app/src/main/java/com/example/testoo/MainActivity.kt
+++ app/src/main/java/com/example/testoo/MainActivity.kt
@@ -2,16 +2,55 @@ package com.example.testoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.testoo.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         FirebaseApp.initializeApp(this)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setUpTabBar()
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container,SignInFragment())
             .commit()
 
+    }
+
+    private fun setUpTabBar() {
+        binding.bottomNavBar.setOnItemSelectedListener{
+            when(it){
+                R.id.icon_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, SignInFragment())
+                        .commit()
+                    true
+                }
+                R.id.icon_location -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, LocationFragment())
+                        .commit()
+                    true
+                }
+                R.id.icon_message-> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MessageFragment())
+                        .commit()
+                    true
+                }
+                R.id.icon_person -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ProfileFragment())
+                        .commit()
+                    true
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/testoo/SignInFragment.kt
+++ app/src/main/java/com/example/testoo/SignInFragment.kt
@@ -40,6 +40,9 @@ class SignInFragment : Fragment() {
                     .addOnCompleteListener{
                         if(it.isSuccessful){
                             Toast.makeText(requireContext(),"LogIn successful",Toast.LENGTH_SHORT).show()
+                            childFragmentManager.beginTransaction()
+                                .replace(R.id.fragment_container, HomeFragment())
+                                .commit()
 
                         }
                         else{
