f515e03487f6558cc4b08f8a9696169fd3ab6101
ElmansouriAMINE <amineelmansouri2001@gmail.com>
2024-04-24 11:48:48 +0100

    update

--- app/src/main/java/com/example/testoo/UI/AgencesFragments/MapsFragment.kt
+++ app/src/main/java/com/example/testoo/UI/AgencesFragments/MapsFragment.kt
@@ -1,7 +1,8 @@
 package com.example.testoo.UI.AgencesFragments
 
-import androidx.fragment.app.Fragment
-
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,36 +10,42 @@ import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.testoo.Domain.Repository.WafaCashRepository
 import com.example.testoo.R
 import com.example.testoo.ViewModels.WafaCashViewModel
-import com.example.testoo.databinding.FragmentLocationBinding
 import com.example.testoo.databinding.FragmentMapsBinding
-
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
 class MapsFragment : Fragment() {
 
     @Inject
     lateinit var wafaCashRepository: WafaCashRepository
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     private lateinit var binding: FragmentMapsBinding
 
@@ -49,6 +56,14 @@ class MapsFragment : Fragment() {
 
 
     private val callback = OnMapReadyCallback { googleMap ->
+//
+//        val casablanca = LatLng(33.589886, -7.603869)
+//        val cameraPosition = CameraPosition.Builder()
+//            .target(casablanca)
+//            .zoom(12f) // Set the zoom level
+//            .build()
+//
+//        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
 
 //        val sydney = LatLng(-34.0, 151.0)
 //        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
@@ -74,10 +89,26 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync { googleMap ->
             this.googleMap = googleMap
-
+            setupMap()
             val casablanca = LatLng(33.5731104, -7.603869)
             googleMap.addMarker(MarkerOptions().position(casablanca).title("Marker in Casablanca"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(casablanca))
+//            googleMap.setOnCameraIdleListener {
+//                val cameraPosition = googleMap.cameraPosition.target
+//                val distance = calculateDistance(currentLatLng, cameraPosition)
+//                if (distance > 100) {
+//                    // Mettre à jour les agences les plus proches
+//                    currentLatLng = cameraPosition
+//                    // Mettre à jour les agences affichées
+//                    updateNearbyAgencies(cameraPosition)
+//                }
+//            }
+//            val cameraPosition = CameraPosition.Builder()
+//                .target(casablanca)
+//                .zoom(14f)
+//                .build()
+
+//            googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(casablanca))
             CoroutineScope(Dispatchers.IO).launch{
                 viewModel.agencesWafaCashState.collectLatest{
                    if (it.isLoading){
@@ -92,23 +123,24 @@ class MapsFragment : Fragment() {
 
             }
 
-            viewModel.agencesWafaCashState
-                .onEach { state ->
-                println("Ptest" + state.agencesWafaCashList.toString())
-                if (!state.isLoading && state.agencesWafaCashList.isNotEmpty()) {
-                    state.agencesWafaCashList.forEach { agence->
-                            val marker = MarkerOptions()
-                                .position(LatLng(agence.latitude.toDouble(), agence.longitude.toDouble()))
-                                .title(agence.nom)
-                            println("hhhhhh" + agence.nom)
-                            googleMap.addMarker(marker)
-                        }
 
-
-                } else if (state.error.isNotEmpty()) {
-                    println("ERRRRRRRRRRRRRROR")
-                }
-            }.launchIn(viewLifecycleOwner.lifecycleScope)
+//            viewModel.agencesWafaCashState
+//                .onEach { state ->
+//                println("Ptest" + state.agencesWafaCashList.toString())
+//                if (!state.isLoading && state.agencesWafaCashList.isNotEmpty()) {
+//                    state.agencesWafaCashList.forEach { agence->
+//                            val marker = MarkerOptions()
+//                                .position(LatLng(agence.latitude.toDouble(), agence.longitude.toDouble()))
+//                                .title(agence.nom)
+//                            println("hhhhhh" + agence.nom)
+//                            googleMap.addMarker(marker)
+//                        }
+//
+//
+//                } else if (state.error.isNotEmpty()) {
+//                    println("ERRRRRRRRRRRRRROR")
+//                }
+//            }.launchIn(viewLifecycleOwner.lifecycleScope)
 
 
 
@@ -124,14 +156,14 @@ class MapsFragment : Fragment() {
             binding.itemGABs.setBackgroundResource(R.drawable.tab_back)
             moveSelectTextView(binding.itemAgences)
 
-            // Handle click for itemAgences
+
         }
 
         binding.itemGABs.setOnClickListener {
             animateTextView(binding.itemGABs)
             moveSelectTextView(binding.itemGABs)
             binding.itemGABs.setBackgroundResource(R.drawable.tab_select)
-            // Handle click for itemGABs
+
         }
 
         binding.itemWafacaches.setOnClickListener {
@@ -139,11 +171,60 @@ class MapsFragment : Fragment() {
             binding.itemGABs.setBackgroundResource(R.drawable.tab_back)
             moveSelectTextView(binding.itemWafacaches)
 
-            // Handle click for itemWafacaches
+            viewModel.agencesWafaCashState
+                .onEach { state ->
+                    println("Ptest" + state.agencesWafaCashList.toString())
+                    if (!state.isLoading && state.agencesWafaCashList.isNotEmpty()) {
+                        state.agencesWafaCashList.forEach { agence->
+                            val marker = MarkerOptions()
+                                .position(LatLng(agence.latitude.toDouble(), agence.longitude.toDouble()))
+                                .title(agence.nom)
+                            println("hhhhhh" + agence.nom)
+                            googleMap.addMarker(marker)
+                        }
+
+
+                    } else if (state.error.isNotEmpty()) {
+                        println("ERRRRRRRRRRRRRROR")
+                    }
+                }.launchIn(viewLifecycleOwner.lifecycleScope)
+
+
         }
 
 
     }
+
+    private fun calculateDistance(start: LatLng, end: LatLng): Float {
+        val results = FloatArray(1)
+        Location.distanceBetween(start.latitude, start.longitude, end.latitude, end.longitude, results)
+        return results[0]
+    }
+
+    @RequiresApi(34)
+    private fun updateNearbyAgencies(center: LatLng) {
+        val maxDistance = 500
+        viewModel.agencesWafaCashState
+            .onEach { state ->
+                if (!state.isLoading && state.agencesWafaCashList.isNotEmpty()) {
+                    googleMap.clear()
+                    state.agencesWafaCashList.forEach { agence ->
+                        val agenceLatLng = LatLng(agence.latitude.toDouble(), agence.longitude.toDouble())
+                        val distance = calculateDistance(center, agenceLatLng)
+                        if (distance <= maxDistance) {
+                            val marker = MarkerOptions()
+                                .position(agenceLatLng)
+                                .title(agence.nom)
+                            googleMap.addMarker(marker)
+                        }
+                    }
+                } else if (state.error.isNotEmpty()) {
+                    println("ERRRRRRRRRRRRRROR")
+                }
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+
 
     private fun moveSelectTextView(targetView: View) {
         val selectTextView = binding.select
@@ -152,6 +233,58 @@ class MapsFragment : Fragment() {
             .x(targetX)
             .setInterpolator(AccelerateDecelerateInterpolator())
             .start()
+    }
+
+//    private fun setupMap() {
+//        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//            googleMap.isMyLocationEnabled = true
+//            googleMap.uiSettings.isMyLocationButtonEnabled = true
+//        } else {
+//            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+//        }
+//    }
+    @RequiresApi(34)
+    @SuppressLint("MissingPermission")
+    private fun setupMap() {
+        val fineLocationPermissionGranted = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        val coarseLocationPermissionGranted = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        if (fineLocationPermissionGranted && coarseLocationPermissionGranted) {
+            googleMap.isMyLocationEnabled= true
+            googleMap.uiSettings.isMyLocationButtonEnabled = true
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        var currentLatLng = LatLng(location.latitude, location.longitude)
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14f))
+                        googleMap.setOnCameraIdleListener {
+                            val cameraPosition = googleMap.cameraPosition.target
+                            val distance = calculateDistance(currentLatLng, cameraPosition)
+                            if (distance > 100) {
+                                currentLatLng = cameraPosition
+                                updateNearbyAgencies(cameraPosition)
+                            }
+                        }
+                    }
+                }
+
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+
+
+    @RequiresApi(34)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setupMap()
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 
 
