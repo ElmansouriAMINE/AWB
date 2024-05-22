0cd2993ab8b699a7204905efce66a1662919dd5e
ElmansouriAMINE <amineelmansouri2001@gmail.com>
2024-05-22 16:20:38 +0100

    update

--- app/src/main/java/com/example/testoo/UI/CardsManagement/TabLayouts/PlafondsMarocFragment.kt
+++ app/src/main/java/com/example/testoo/UI/CardsManagement/TabLayouts/PlafondsMarocFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import com.example.testoo.R
 import com.example.testoo.databinding.FragmentPlafondsMarocBinding
 
@@ -19,6 +20,59 @@ class PlafondsMarocFragment : Fragment() {
     ): View? {
 
         binding= FragmentPlafondsMarocBinding.inflate(layoutInflater)
+
+
+        var isSeekBarChanging = false
+
+        binding.seekBarMontantRetraitMaroc.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    binding.montantRetraitMaroc.setText("$progress")
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                isSeekBarChanging = true
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                isSeekBarChanging = false
+            }
+        })
+
+        binding.seekBarMontantInternetMaroc.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    binding.montantInternetMaroc.setText("$progress")
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                isSeekBarChanging = true
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                isSeekBarChanging = false
+            }
+        })
+
+        binding.seekBarMontantTPEMaroc.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    binding.montantTPEMaroc.setText("$progress")
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                isSeekBarChanging = true
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                isSeekBarChanging = false
+            }
+        })
+
+
 
 
         return binding.root
